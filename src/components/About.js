0062dd64b84cba39f83929cfec5fd1fa0916ef64
rohitsainier <rohitsainier@gmail.com>
2023-10-30 14:33:58 +0530

    test about info added

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 const About = () => {
-  return <section id="about">{/* Your about content goes here */}</section>;
+  return (
+    <section id="about">
+      <div className="about-content">
+        <h2>Hello! My name is Rohit.</h2>
+        <p>
+          I like making things that are accessible online. When my brother gave
+          me a MacBook Air in 2016, I became interested in iOS development and
+          began creating little applications for amusement.
+        </p>
+        <p>
+          Today, having started my career with a small startup, a rapid-growth
+          business, and a large corporation has been a privilege. Building
+          inclusive, accessible products and digital experiences for Majid Al
+          Futtaim is my main emphasis right now.
+        </p>
+        <p>I've lately worked with the following technologies:</p>
+        <ul>
+          <li>SwiftUI</li>
+          <li>Combine</li>
+          <li>Swift</li>
+          <li>Go</li>
+          <li>Unit Testing</li>
+          <li>XCUITest</li>
+          <li>WebRTC</li>
+          <li>Firebase</li>
+          <li>Fastlane</li>
+          <li>Bash Scripting</li>
+          <li>MVVM</li>
+          <li>VIPER</li>
+          <li>Github Actions</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default About;
